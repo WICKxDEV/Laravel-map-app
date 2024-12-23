4afb4c365d81fc2f5b78829f8d33072d1a48f61a
WICKxDEV <160169165+WICKxDEV@users.noreply.github.com>
2024-12-23 23:16:49 +0530

    Update LocationController

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Location;
 use Illuminate\Http\Request;
 
 class LocationController extends Controller
 {
-    //
+    public function index()
+    {
+        $locations = Location::all();
+        return view('locations.index', compact('locations'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
+        ]);
+
+        Location::create($request->only(['name', 'latitude', 'longitude']));
+
+        return redirect()->route('locations.index');
+    }
 }
